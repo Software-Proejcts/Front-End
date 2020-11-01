2a74763d62b398cc95848973858f645e2907f810
danyal.murtaza <danyal.murtaza1@gmail.com>
2020-10-31 20:45:23 -0500

    added dropdown option for Schools.

--- ReactLoginRegisterUI-master/src/components/signup.component.js
+++ ReactLoginRegisterUI-master/src/components/signup.component.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import {Link} from 'react-router-dom';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 
+const options = [
+    'DePaul University'
+];
+const defaultOption =Option[0];
 export default class SignUp extends Component {
     render() {
         return (
@@ -26,6 +32,11 @@ export default class SignUp extends Component {
                 <div className="form-group">
                     <label>Password</label>
                     <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <div className="form-group">
+                    <label>School</label>
+                  <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
                 </div>
 
                 <button type="submit" className="btn btn-dark btn-lg btn-block"><Link to="/signupt">Register</Link></button>
